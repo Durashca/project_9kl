2ef739fcd0f9a151ebf20668e0c5964cbec140a1
Almas <dubkow.904rtw.tus@gmail.com>
2024-01-15 23:00:50 +0300

    Added switching between login and registration

--- src/js/authorization.js
+++ src/js/authorization.js
@@ -1,17 +1,4 @@
 authorization = {
-    AddEventRegistry() {
-        let form = document.querySelector("#form-profile");
-        form.addEventListener("submit", function (event) {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-                authorization.Register();
-            }
-
-            form.classList.add("was-validated");
-        }, false);
-    },
-
     Register() {
         console.log("Вы зарегестрированы");
     },
@@ -20,5 +7,3 @@ authorization = {
 
     },
 };
-
-authorization.AddEventRegistry();
