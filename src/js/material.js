91ba5f76b27bf204f0f6a76222a324ced6f32fdb
Almas <dubkow.904rtw.tus@gmail.com>
2024-01-25 22:58:22 +0300

    Added sub-lessons, and their collapse and expansion

--- src/js/material.js
+++ src/js/material.js
@@ -5,40 +5,71 @@ let material = (function () {
         let materialElem = document.querySelector(".list-lessons");
         for (let elem of lessonElems) {
             materialElem.appendChild(elem.lessonButton);
+            showChildrenLessons(elem);
+        }
+    }
+
+    function showChildrenLessons(parent) {
+        let materialElem = document.querySelector(".list-lessons");
+        for (let elem of parent.children) {
+            materialElem.appendChild(elem.lessonButton);
+            showChildrenLessons(elem);
         }
     }
 
     function createLessons(lessonsInfo) {
         let i = 0;
         for (let lesson of lessonsInfo) {
-            let lessonElem = createLesson(lesson, i++);
+            let lessonElem = createLesson(lesson, (++i).toString());
             lessonElems.push(lessonElem);
         }
     }
 
-    function createLesson(lessonInfo, index) {
-        let lessonCode =
+    function createLesson(lessonInfo, number, indent = 0) {
+        const lessonCode =
         `
             <div class="left-part-lesson">
-                <a class="number-lesson">Урок ${index + 1}</a>
+                <a class="number-lesson">Урок ${number}</a>
             </div>
-            <div class="right-part-lesson">
+            <div class="middle-part-lesson">
                 <h6 class="text-lesson name-lesson">${lessonInfo.name}</h6>
                 <p class="text-lesson description-lesson">${lessonInfo.description}</p>
             </div>
+            <div class="right-part-lesson">
+            </div>
         `;
 
+        const codeFolding =
+            `<button type="button" class="btn btn-primary folding" data-bs-toggle="button" aria-pressed="false">▼</button>`;
+
         let lesson = createElement("a", "btn btn-primary lesson");
+        lesson.style.marginLeft = `${indent}px`;
         lesson.innerHTML = lessonCode;
 
         let leftPart = lesson.querySelector(".left-part-lesson");
-        leftPart.insertAdjacentHTML('beforeend', stateElems[lessonInfo.state]);
+        let rightPart = lesson.querySelector(".right-part-lesson");
+
+        if (lessonInfo.children.length > 0)
+            rightPart.insertAdjacentHTML('afterbegin', codeFolding);
+
+        if (lessonInfo.state in stateToLessonNumberElement)
+            leftPart.insertAdjacentHTML('beforeend', stateToLessonNumberElement[lessonInfo.state]);
+
+        stateToAdditionalDecoration[lessonInfo.state]?.call(null, lesson);
 
         let name = lesson.querySelector(".name-lesson");
         let description = lesson.querySelector(".description-lesson");
         let numberLesson = lesson.querySelector(".number-lesson");
 
-        return new LessonElement(lesson, name, description, numberLesson, lessonInfo.link);
+        let lessonElem = new LessonElement(lesson, name, description, numberLesson, lessonInfo.link);
+
+        let i = 0;
+        for (let lessonChild of lessonInfo.children) {
+            let childElem = createLesson(lessonChild, `${number}.${++i}`, indent + 20);
+            lessonElem.children.push(childElem);
+        }
+
+        return lessonElem;
     }
 
     function createElement(tagName, className, parent = null) {
--- src/js/material_settings.js
+++ src/js/material_settings.js
@@ -1,33 +1,74 @@
 const lessonState = { IDLE: 'idle', NEW: 'new', SOON: 'soon', OLD: 'old' };
 
-function LessonInfo(name, description, link, state = 'idle') {
+function LessonInfo(name, description, link, state = 'idle', children = []) {
     this.name = name;
     this.description = description;
     this.state = state;
     this.link = link;
+    this.children = children;
 }
 
-function LessonElement(lessonButton, name, description, numberLesson, link) {
+function LessonElement(lessonButton, name, description, numberLesson, link, children = []) {
     this.lessonButton = lessonButton;
     this.name = name;
     this.description = description;
     this.numberLesson = numberLesson;
     this.link = link;
+    this.children = children;
 
-    lessonButton.href = link;
+    this.folding = lessonButton.querySelector(".folding");
+    this.folding?.addEventListener('click', () => this.expanded = !this.expanded);
+
+    this._expanded = true;
+
+    Object.defineProperty(this, 'expanded', {
+        get: function() {
+            return this._expanded;
+        },
+        set: function(value) {
+            this._expanded = value;
+
+            if (this.folding === null)
+                return;
+
+            const toggleIcons = ['▼', '▲'];
+            this.folding.innerText = toggleIcons[+value];
+            this.switchVisibilityChildren(value);
+        }
+    });
+
+    this.switchVisibilityChildren = function (value) {
+        for (let child of children) {
+            const visibilityParams = ['none', ''];
+
+            child.lessonButton.style.display = visibilityParams[+value];
+            child.switchVisibilityChildren(value);
+        }
+    };
+
+    if (link !== "")
+        lessonButton.href = link;
 }
 
 let lessons = [
-    new LessonInfo("Горячие клавиши", "Упрощение жизни с помощью горячих клавиш" , "hotkeys.html"),
+    new LessonInfo("Горячие клавиши", "Упрощение жизни с помощью горячих клавиш" , "hotkeys.html", "idle", [
+        new LessonInfo("Тест1", "Описание1", ''),
+    ]),
     new LessonInfo("Скриншоты", "Для чего нужны и как их делать?", "screenshots.html"),
     new LessonInfo("Перекидывание файлов", "Как перекинуть файлы с телефона на ПК?", "", "soon"),
 ];
 
-const stateElems = {
-    'idle': ``,
+const stateToLessonNumberElement = {
     'new': `<a class="number-lesson bg-success">Новый</a>`,
     'soon': `<a class="number-lesson bg-primary">Скоро</a>`,
     'old': `<a class="number-lesson bg-warning">Устар.</a>`,
+};
+
+const stateToAdditionalDecoration = {
+    'soon': (lesson) => {
+        lesson.style.pointerEvents = 'none';
+        lesson.classList.add('text-muted');
+    },
 };
 
 material.createLessons(lessons);
