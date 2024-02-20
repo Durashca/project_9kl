5edd101ea3204c5556ac63660c718881e2716984
Durashca <144939800+Durashca@users.noreply.github.com>
2024-02-21 01:02:30 +0300

    added task checking

---
+++ src/js/transition.js
@@ -0,0 +1,13 @@
+/* кнопка перехода*/
+let btn_move_to_next = document.getElementById('createPageButton');
+/*кнопка задания*/
+let btn_task_check = document.getElementById('btn-task-check')
+
+/*функция для добавления/удаления класса disabled */
+function func_tg_dis() {
+    btn_move_to_next.classList.toggle('disabled')
+}
+
+/*вызов по умолчанию*/
+func_tg_dis();
+
