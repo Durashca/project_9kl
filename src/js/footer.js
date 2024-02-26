ae06db08e2ee94ac4c85583656d35745fda1e6c3
Durashca <144939800+Durashca@users.noreply.github.com>
2024-02-27 00:44:47 +0300

    adding footer

---
+++ src/js/footer.js
@@ -0,0 +1,8 @@
+let footerHTML = `<footer class="bd-footer py-4  mt-5s bg-body-tertiary">
+    <div class="container">
+        <p>Сайт <strong>ВсеЗнай</strong> &copy; 2024</p>
+        <p>Сделано Космылёвым Владиславом Евгеньевичем</p>
+    </div>
+</footer>`;
+
+document.body.insertAdjacentHTML('beforeend', footerHTML);
