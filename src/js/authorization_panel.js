eb7277d13d16c46d0b69100d2f5dee74ed46e48e
Almas <dubkow.904rtw.tus@gmail.com>
2024-02-29 21:42:15 +0300

    Removed unnecessary garbage in authorization_panel.js

--- src/js/authorization_panel.js
+++ src/js/authorization_panel.js
@@ -21,9 +21,8 @@ function addInputValidationEvent() {
     forms.forEach(function (form) {
         form.addEventListener("submit", function (event){
             event.preventDefault();
-            if (!form.checkValidity()) {
+            if (!form.checkValidity())
                 event.stopPropagation();
-            }
 
             let correctly = Array.from(form.children).every((elem) => {
                 let input = elem.querySelector("input");
@@ -34,52 +33,12 @@ function addInputValidationEvent() {
                 return true;
             })
 
-            if (correctly) {
+            if (correctly)
                 form.classList.contains("registry") ? authorization.register() : authorization.login();
-            }
 
             form.classList.add("was-validated");
         }, false);
     });
-}
-
-function addPasswordsValidateEvent() {
-    let forms = document.querySelectorAll(".form-profile");
-
-    for (let form of forms) {
-        form.addEventListener("submit", function (event) {
-            validatePassword(form, event);
-        });
-    }
-}
-
-function validatePassword(form, event) {
-    let passwordElem = Array.from(form.children).find((elem) => {
-        let input = elem.querySelector("input");
-
-        if (input !== null)
-            return input.type === "password";
-
-        return false;
-    });
-    let passwordInput = passwordElem.querySelector("input");
-    let passwordFeedback = passwordElem.querySelector(".invalid-feedback");
-    let password = passwordInput.value;
-
-    if (password.length === 0)
-        return;
-
-    if (password.length < 8) {
-        passwordFeedback.innerText = "Пароль должен содержать не менее 8 символов";
-        event.preventDefault();
-        return;
-    }
-
-    let passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    if (!passwordRegex.test(password)) {
-        passwordFeedback.innerText = "Пароль должен содержать цифры, специальные символы, строчные и заглавные буквы";
-        event.preventDefault();
-    }
 }
 
 function addSwitchingAuthorizationCapabilitiesEvent() {
@@ -91,10 +50,7 @@ function addSwitchingAuthorizationCapabilitiesEvent() {
     textRegistry.addEventListener("click", () =>
         authorizationPanel.switchTo(0));
 
-    // addPasswordsValidateEvent();
     addInputValidationEvent();
 }
-
-// addSwitchingAuthorizationCapabilitiesEvent();
 
 export { authorizationPanel, addSwitchingAuthorizationCapabilitiesEvent };
