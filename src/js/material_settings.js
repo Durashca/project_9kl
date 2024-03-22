a626fcb895dc53715d8c060c93009bd0a959d3e8
Durashca <144939800+Durashca@users.noreply.github.com>
2024-03-23 00:54:57 +0300

    Improved tasks in the lesson with sound and function keys

--- src/js/material_settings.js
+++ src/js/material_settings.js
@@ -18,10 +18,10 @@ let lessons = [
 
     new LessonInfo("Логин", "Разбираем понятия: авторизация, регистрация, аутентификация и тд.", "the_memo.html"),
     new LessonInfo("Глобальные горячие клавиши", "Вставить, копировать, вырезать, отменить последнее действие", "hotkeys.html"),
+    new LessonInfo("Функциональные клавиши", "Учимся быстро перемещаться по тексту", "basic_keys.html"),
     new LessonInfo("F клавиши", "Настройка звука, яркости, видео", "f_keys.html"),
     new LessonInfo("Скриншоты", "Делаем скрин на пк(ноутбуке)", "screenshots.html"),
     new LessonInfo("Перенос файла через USB-подключение", "Изучаем способы переноса файлов, документов, изображений", "transferring_files.html"),
-    new LessonInfo("Функциональные клавиши", "Учимся быстро перемещаться по тексту", "basic_keys.html"),
     new LessonInfo("Скачивание приложения", "Скачиваем приложения через браузер", "download_the_application.html"),
     new LessonInfo("Удаление приложения", "Удаляем ненужные программы", "uninstalling_an_application.html"),
     new LessonInfo("Ошибки со звуком", "Узнаем разные способы исправления ошибок с воспроизведением звука", "audio_errors.html"),
