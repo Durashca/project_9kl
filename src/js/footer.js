205b8e68411ec9ffef6fb71ab9a95cc4da2e63dd
Durashca <144939800+Durashca@users.noreply.github.com>
2024-04-01 19:49:14 +0300

    text corrected

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const footerHTML = `<footer class="bd-footer py-4 mt-5s bg-body-tertiary">
     <div class="container">
-        <p>Сайт ВсеЗнай 2024</p>
+        <p>ВсеЗнай 2024</p>
         <p>&copy; <strong>Космылев Владислав Евгеньевич</strong></p>
     </div>
 </footer>`;
