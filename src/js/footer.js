90cf21b83020b6e109e0cce29d8b5fd34809baf1
Durashca <144939800+Durashca@users.noreply.github.com>
2024-04-01 19:48:39 +0300

    moving the div to the end of the body

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,8 +1,13 @@
-let footerHTML = `<footer class="bd-footer py-4  mt-5s bg-body-tertiary">
+document.addEventListener('DOMContentLoaded', function() {
+    const footerHTML = `<footer class="bd-footer py-4 mt-5s bg-body-tertiary">
     <div class="container">
-        <p>Сайт <strong>ВсеЗнай</strong> &copy; 2024</p>
-        <p>Сделано Космылёвым Владиславом Евгеньевичем</p>
+        <p>Сайт ВсеЗнай 2024</p>
+        <p>&copy; <strong>Космылев Владислав Евгеньевич</strong></p>
     </div>
 </footer>`;
 
-document.body.insertAdjacentHTML('beforeend', footerHTML);
+    const footerElement = document.createElement('div');
+    footerElement.innerHTML = footerHTML;
+
+    document.body.appendChild(footerElement);
+});
